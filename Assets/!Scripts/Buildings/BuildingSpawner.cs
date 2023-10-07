526efb8c8c0958267765ae3f7f3310cb6e1063d2
Lost_Shadow <sobolm58@gmail.com>
2023-10-07 12:56:27 +0700

    set new buildings

--- Assets/!Scripts/Buildings/BuildingSpawner.cs
+++ Assets/!Scripts/Buildings/BuildingSpawner.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Extensions;
+using Unity.Mathematics;
 
 public class BuildingSpawner : MonoBehaviour
 {
-    [SerializeField] private Vector3[] buildingSpawnPoints;
+    [SerializeField] private List<Transform> buildingSpawnPoints;
+    [SerializeField] private List<GameObject> _testNewBuldings;
     [SerializeField] private Building buildingPrefab;
     [SerializeField] private List<int> smallBuildingIndexes;
 
@@ -13,15 +16,22 @@ public class BuildingSpawner : MonoBehaviour
 
     public void Launch(BuildingSpawnConfig buildingSpawnConfig)
     {
-        _buildingSpawnConfig = buildingSpawnConfig;
-
-        for (int i = 0; i < buildingSpawnPoints.Length; i++)
+        // _buildingSpawnConfig = buildingSpawnConfig;
+        //
+        // for (int i = 0; i < buildingSpawnPoints.Count; i++)
+        // {
+        //     Building building = Instantiate(buildingPrefab, transform.position + buildingSpawnPoints[i], Quaternion.identity, transform);
+        //     
+        //     _isOnLeftStreetSide = buildingSpawnPoints[i].x < 0;
+        //
+        //     building.Launch(_buildingSpawnConfig, smallBuildingIndexes, i, _isOnLeftStreetSide);
+        // }
+        for (int i = 0; i < buildingSpawnPoints.Count; i++)
         {
-            Building building = Instantiate(buildingPrefab, transform.position + buildingSpawnPoints[i], Quaternion.identity, transform);
-            
-            _isOnLeftStreetSide = buildingSpawnPoints[i].x < 0;
-
-            building.Launch(_buildingSpawnConfig, smallBuildingIndexes, i, _isOnLeftStreetSide);
+            Instantiate(_testNewBuldings.RandomItem(),
+                buildingSpawnPoints[i].position,
+                buildingSpawnPoints[i].position.x < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity,
+                transform);
         }
     }
 }
